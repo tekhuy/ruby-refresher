6a426db447b7ef03682947c6e5489b7c625bec62
tekhuy <huyle20@hotmail.com>
2015-02-06 17:48:17 +0000

    first 3 tests passed

--- lib/questions.rb
+++ lib/questions.rb
@@ -1,13 +1,16 @@
 # keep only the elements that start with an a
 def select_elements_starting_with_a(array)
+  n = array.select{|i| i =~ /^[a]/ }
 end
 
 # keep only the elements that start with a vowel
 def select_elements_starting_with_vowel(array)
+  n = array.select{|i| i =~ /^[a, e, i, o ,u]/ }
 end
 
 # remove instances of nil (but NOT false) from an array
 def remove_nils_from_array(array)
+  n = array.compact
 end
 
 # remove instances of nil AND false from an array
