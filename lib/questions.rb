8e1777c1e4d76548a4d749bf5a702c5b300ecd46
tekhuy <huyle20@hotmail.com>
2015-02-07 19:49:42 +0000

    13 passed

--- lib/questions.rb
+++ lib/questions.rb
@@ -77,6 +77,8 @@ end
 # e.g. 'bob'. So in the array ['bob', 'radar', 'eat'], there
 # are 2 palindromes (bob and radar), so the method should return 2
 def number_of_elements_that_are_palindromes(array)
+  n = array.select{|i| i == i.reverse}
+  n.size
 end
 
 # return the shortest word in an array
